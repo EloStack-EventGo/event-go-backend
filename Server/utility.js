4ba0e815e7292662bb2e03f5e024b54ac75f5fcb
yashaswi <yashaswi.kul@gmail.com>
2024-07-02 22:36:53 -0700

    fixed bugs

--- Server/utility.js
+++ Server/utility.js
@@ -1,4 +1,4 @@
-import { supabaseAdminClient } from "../Database/schema.js";
+import { supabaseAdminClient } from "../Database/Schema/schema.js"
 
 //Response wrapper for the server
 export class ServerResponse{
