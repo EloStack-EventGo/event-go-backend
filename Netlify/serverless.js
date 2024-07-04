55857ba8170ffa3c7ce33ab2b86332489a3639e1
yashaswi <yashaswi.kul@gmail.com>
2024-07-04 12:09:51 -0700

    chores

--- Netlify/serverless.js
+++ Netlify/serverless.js
@@ -5,5 +5,7 @@ import { serverless } from "serverless-http"
 
 //Sets app to use router1
 expressServer.router('router1')
+expressServer.use_cors(false)
+expressServer.update_router('router1', {router_url:"Backend/Database/Netlify/serverless"})
 module.exports.handler=serverless(expressServer.app())
 
